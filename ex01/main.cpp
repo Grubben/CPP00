5015cbda9372e1971de617c6c8fa64bd9637a0ad
Grubben <antoniomsprc@gmail.com>
2023-02-09 15:31:55 +0000

    Made sure input is ADD, ...

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -16,18 +16,21 @@ int main(void)
 		std::cout << "> ";
 		// input.clear();
 		std::getline(std::cin, input);
-		if (input == "ADD" || input == "a")
+		// if (input == "ADD" || input == "a")
+		if (input == "ADD")
 		{
 			std::cout << "Adding!" << std::endl;
 			phonebook.addContact();
 			// input.clear();
 		}
-		else if (input == "SEARCH" || input == "s")
+		// else if (input == "SEARCH" || input == "s")
+		else if (input == "SEARCH")
 		{
 			std::cout << "Searching!" << std::endl;
             phonebook.show();
 		}
-		else if (input == "EXIT" || input == "e")
+		// else if (input == "EXIT" || input == "e")
+		else if (input == "EXIT")
             break;
         // else
 		// 	break;
